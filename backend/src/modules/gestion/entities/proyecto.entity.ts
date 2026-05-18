e5443141cbeb08466ba38c2ea17af4ac87ff6612
Benja <benjafibiger@gmail.com>
2026-05-18 13:47:31 -0300

    correcion de errores

---
+++ backend/src/modules/gestion/entities/proyecto.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { EstadosProyectosEnum } from '../enums/estados-proyectos.enum';
+import { ClienteEntity } from './cliente.entity';
+import { TareaEntity } from './tarea.entity';
+
+@Entity('proyectos')
+export class ProyectoEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text', unique: true, nullable: false })
+  nombre!: string;
+
+  @Column({ type: 'enum', enum: EstadosProyectosEnum, default: EstadosProyectosEnum.ACTIVO })
+  estado!: EstadosProyectosEnum;
+
+  // Dejamos ÚNICAMENTE la relación para que TypeORM no se confunda al insertar
+  @ManyToOne(() => ClienteEntity, (cliente) => cliente.proyectos, { nullable: true })
+  @JoinColumn({ name: 'id_cliente' })
+  cliente!: ClienteEntity;
+
+  @OneToMany(() => TareaEntity, (tarea) => tarea.proyecto)
+  tareas!: TareaEntity[];
+}
